8b9489d1aa138ff7d4e4f42cbba9f5512acf341f
robergit88 <pumi07@gmail.com>
2021-07-06 17:15:43 +0000

    zapato

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -9,19 +9,25 @@ sap.ui.define([
 
         return Controller.extend("packinvoice.invoices.controller.MainView", {
             onInit: function () {
+                //Se define un objeto modelo JSON
                 const oJSONModel = new sap.ui.model.json.JSONModel();
                 const oView = this.getView();
+                //el modelo es vinculado para que tome valor de un fichero 
                 oJSONModel.loadData("./model/SelectionScreenMenu.json");
+                //el modelo se llamará
                 oView.setModel(oJSONModel, "selectionScreen");
             },
 
-            onFilter: function(oEvent) {
+            onFilter: function (oEvent) {
             },
-            onClearFilter: function() {
+            onClearFilter: function () {
+                //de todos los modelos que puede tener una vista
+                //obtenemos el que nos interesa
+                const oModelSelScreen = this.getView().getModel("selectionScreen");
                 //una vista puede tener muchos modelos
-                //La vista interpreta el modelo y se actualiza
-                const oModelSelScreen = this.getView().getModel("selectionScreen"); 
+                //La vista interpreta el modelo y se actualiza                
                 //CountryKey es la clave que se actualiza en el modelo SelectionScreenMenu
+                oModelSelScreen.setProperty("/ShipName", "");
                 oModelSelScreen.setProperty("/CountryKey", "");
             }
         });
